e63a18163c7f853d9e01bd11704378ed7bcf624c
takahashi <coudai3141xyzww@gmail.com>
2025-07-21 14:35:46 +0900

    login

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', [AuthController::class, 'index']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [AuthController::class, 'index']);
+    });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
